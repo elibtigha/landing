a382bbbb1a81670febe45ad848096ac1fdedce26
Elizabeth Lin <t-ellin@microsoft.com>
2019-07-12 10:21:27 -0700

    yeet

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-// const Http = new XMLHttpRequest();
-// const url='https://github.com/login/oauth/authorize?client_id=Iv1.73333a21c7762914';
-// Http.open("GET", url);
-// Http.send();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const Http = new XMLHttpRequest();
+const url='https://github.com/login/oauth/authorize?client_id=Iv1.73333a21c7762914';
+Http.open("GET", url);
+Http.send();
 
-// Http.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         console.log(Http.responseText)
-//     }
-// }
+Http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(Http.responseText)
+    }
+}
+
+// app.use(cors())
