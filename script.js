e3768926ed8c33b673e5248a09761746e2110e5f
Elizabeth Lin <t-ellin@microsoft.com>
2019-07-12 15:57:11 -0700

    hello

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const url='https://github.com/login/oauth/authorize?client_id=Iv1.73333a21c7762914';
+$(document).ready(function(){
+  $("button").click(function(){
+    $.get( url, function(data, status){
+      console.log(`${data}`)
+    });
+  });
+});
+
+
 
 // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const Http = new XMLHttpRequest();
-const url='https://github.com/login/oauth/authorize?client_id=Iv1.73333a21c7762914';
 Http.open("GET", url);
 Http.send();
-
 Http.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         console.log(code)
