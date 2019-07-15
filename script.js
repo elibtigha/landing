cfc170f37cfd4e975befe03ea775707b7ad95dd5
Elizabeth Lin <t-ellin@microsoft.com>
2019-07-15 09:38:04 -0700

    good

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const url='https://github.com/login/oauth/authorize?client_id=Iv1.73333a21c7762914';
 
-$("button").click(function(){
-$.get( url, function(data, status){
-    console.log(`${data}`)
-});
-});
+// $("button").click(function(){
+//     $.get(url, function(data, status){
+//         console.log(`${data}`)
+//     });
+// });
 
+$(document).ready(function() {
+    const Url = 'https://jsonplaceholder.typicode.com/posts';
+    $('button').click(function() {
+        $.ajax({
+            url: Url,
+            type: "GET",
+            success: function(result) {
+                console.log(result)
+            },
+            error: function(error) {
+                console.log(`Error ${error}`)
+            }
+        })
+    })
+})
 
-
-
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const Http = new XMLHttpRequest();
 Http.open("GET", url);
 Http.send();
@@ -18,5 +30,3 @@ Http.onreadystatechange = function() {
         console.log(code)
     }
 }
-
-// app.use(cors())
